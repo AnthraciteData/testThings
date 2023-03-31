5230fb9cbcdba01c8bff5f6d3f82ad8cd659c9e2
Javier Sanchez <48665751+AnthraciteData@users.noreply.github.com>
2023-03-30 18:02:25 -0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+    devtool: 'eval-source-map',
+    mode: 'development',
+    context : path.join(__dirname,'src'),
+    entry: {
+        SignupLogin : './Signup-Login/UserFunctions.js',
+        resetPassword :"./resetPassword/resetPassword.js",
+        homepageElements : "./homePage/homePageElements.js",
+        JobOpeings : './JobOpenings/JobOpenings.js',
+        testAPI : './JobOpenings/JsearchAPI.js'
+        // fireBaseint: "/firebaseInitilization.js"
+    },
+    output:{
+
+        path: path.resolve(__dirname,'dist'),
+        filename : '[name].bundle.js'
+        
+    },
+
+    watch : true
+
+
+}
